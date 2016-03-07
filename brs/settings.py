c170ff37c1f3b0471af2041bfaf9d54be0c1c1e4
AECUTE <kampee.expertly01@gmail.com>
2016-03-08 00:05:42 +0700

    init

--- brs/settings.py
+++ brs/settings.py
@@ -138,6 +138,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
